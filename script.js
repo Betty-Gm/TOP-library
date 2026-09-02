e4b7c09d2ec70fd80b2ae94c67fb837ccb82306a
bethlehem <bethlehemgebremichael11@gmail.com>
2026-09-02 14:02:59 +0300

    Implement add-book form and refactor displayBooks to clear-and-rebuild

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary= [];
+let bookContainer= document.querySelector(".book-container");
 
 function Book(title, author, page, read){
     this.id= crypto.randomUUID();
@@ -11,18 +12,16 @@ function Book(title, author, page, read){
 function addBookToLibrary(title, author, page, read){
     
     myLibrary.push(new Book(title, author, page, read));
+    console.log(displayBooks(myLibrary));
 }
 
-// manually adding some books to see the display
-console.log(addBookToLibrary("title", "author", 125, "Already Read"));
-console.log(addBookToLibrary("title2", "author", 125, "Already Read"));
 
-let bookContainer= document.querySelector(".book-container");
 
 function displayBooks(myLibrary){
     
+    bookContainer.textContent= "";
     for(let a=0; a<myLibrary.length; a++){
-
+       
         let bookCard= document.createElement("div");
         let list= document.createElement("ul");
         let title= document.createElement("li");
@@ -44,8 +43,64 @@ function displayBooks(myLibrary){
 
     }
 
+}
+
+
+
+// adding a "new book" button
+function renderForm(){
+    const sidebar= document.querySelector(".sidebar");
+    sidebar.innerHTML= `
+      <form id= "dynamic-form" action= "#" method= "post">
+        <h3>New Book</h3>
+        <label for="titleid">Title</label>
+        <input type="text" id="titleid">
+        <label for="authorid">Author</label>
+        <input type="text" id="authorid">
+        <label for="pageid">Number of pages</label>
+        <input type="number" name="" id="pageid">
+        <label for="readid">reading status</label>
+        <input type="text" id="readid">
+
+
+        <button id="submit-id" type= "button">Submit</button>
+        <button id = "cancel-id">Cancel</button>
+
+        
+    </form>
+    `
+
+    const submitBtn= document.getElementById("submit-id");
+    const cancelBtn= document.getElementById("cancel-id");
+    
+
+    const titleBtn= document.getElementById("titleid");
+    const authorBtn= document.getElementById("authorid");
+    const pageBtn= document.getElementById("pageid");
+    const readBtn= document.getElementById("readid");
+    submitBtn.addEventListener("click", (event)=>{
+        event.preventDefault();
+        addBookToLibrary(titleBtn.value, authorBtn.value, pageBtn.value, readBtn.value);
+        titleBtn.value="";
+        authorBtn.value= "";
+        pageBtn.value= 0;
+        readBtn.value= "";
+
+    });
+    cancelBtn.addEventListener("click", (event)=>{
+        event.preventDefault();
+        titleBtn.value="";
+        authorBtn.value= "";
+        pageBtn.value= "";
+        readBtn.value= "";
+    })
 
 
 
 }
-console.log(displayBooks(myLibrary));
+
+ const newBookBtn= document.getElementById("newbook-id");
+ newBookBtn.addEventListener("click", ()=>{
+    renderForm();
+
+ });
